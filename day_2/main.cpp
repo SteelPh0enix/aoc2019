9482df33d40d7af7e87e64f5f59cefc1e6e8633e
SteelPh0enix <steelphoenix2137@gmail.com>
2019-12-02 22:58:08 +0100

    Day 2: first part done

--- day_2/main.cpp
+++ day_2/main.cpp
@@ -1,3 +1,84 @@
+#include <file_reader.hpp>
+#include <fstream>
 #include <iostream>
+#include <iterator>
+#include <vector>
 
-int main() {}
+struct Intcode {
+  int opcode;
+  int input_positions[2];
+  int output_position;
+};
+
+template <typename It>
+Intcode readNextIntcode(It& input_iterator) {
+  Intcode instruction{};
+
+  instruction.opcode = *input_iterator++;
+
+  if (instruction.opcode != 99) {
+    instruction.input_positions[0] = *input_iterator++;
+    instruction.input_positions[1] = *input_iterator++;
+    instruction.output_position = *input_iterator++;
+  }
+
+  return instruction;
+}
+
+bool executeIntcode(std::vector<int>& data, Intcode const& instruction) {
+  int first_input = data.at(instruction.input_positions[0]);
+  int second_input = data.at(instruction.input_positions[1]);
+  int& output = data.at(instruction.output_position);
+
+  switch (instruction.opcode) {
+    case 1: {
+      output = first_input + second_input;
+      break;
+    }
+    case 2: {
+      output = first_input * second_input;
+      break;
+    }
+
+    case 99: {
+      std::cout << "Halt opcode received, exiting...\n";
+      return false;
+    }
+
+    default: {
+      std::cout << "Unknown opcode " << instruction.opcode
+                << " received, exiting...\n";
+      return false;
+    }
+  }
+
+  return true;
+}
+
+int main() {
+  std::fstream input_file("input.txt", std::ios::in);
+
+  if (!input_file.is_open()) {
+    std::cout << "Couldn't open data file!\n";
+    return 1;
+  }
+
+  std::vector<int> data{};
+  readSeparatedData(input_file, ',', std::back_inserter(data),
+                    [](std::string const& s) { return std::stoi(s); });
+
+  data[1] = 12;
+  data[2] = 2;
+
+  auto intcode_iterator = data.begin();
+  Intcode instruction{};
+  do {
+    instruction = readNextIntcode(intcode_iterator);
+  } while (executeIntcode(data, instruction));
+
+  std::cout << "Finished state: ";
+  for(auto e: data) {
+    std::cout << e << ' ';
+  }
+  std::cout << std::endl;
+}
--- utils/file_reader.hpp
+++ utils/file_reader.hpp
@@ -14,3 +14,19 @@ std::size_t readDataLines(std::istream& source,
 
   return lines;
 }
+
+template <typename OutputIt, typename ConvertF>
+std::size_t readSeparatedData(std::istream& source,
+                              char separator,
+                              OutputIt output,
+                              ConvertF convert) {
+  std::size_t data_amount{0};
+
+  for (std::string data_sample{};
+       std::getline(source, data_sample, separator);) {
+    output = convert(data_sample);
+    data_amount++;
+  }
+
+  return data_amount;
+}
